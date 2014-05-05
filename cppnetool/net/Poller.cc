66efd8e5e12ae585472d0860a83e7d2d1c998365
root <root@ww-OptiPlex-7010.(none)>
2014-05-05 21:00:08 +0800

    20140504

--- cppnetool/net/Poller.cc
+++ cppnetool/net/Poller.cc
@@ -35,7 +35,40 @@ void Poller::fillActiveChannels(int numEvents, ChannelList *activeChannels) cons
 		if (pfd->revents > 0)
 		{
 			--numEvents;
-			
+			ChannelMap::cosnt_iterator ch = channels_.find(pfd->fd);
+			assert(ch != channels_.end());
+			Channel *channel = ch->second;
+			assert(channel->fd() == pfd->fd);
+			channel->set_revents(pfd->revents);
+			activeChannels->push_back(channel);
+		}
+	}
+}
+
+void Poller::updateChannel(Channel *channel)
+{
+	assertInLoopThread();
+	if (channel->index() < 0) {
+		assert(channels_.find(channel->fd()) == channels_end());
+		struct pollfd pfd;
+		pfd.fd = channel->fd();
+		pfd.events = static_cast<short>(channel->events());
+		pfd.revents = 0;
+		pollfd_.push_back(pfd);
+		int idx = static_cast<int>(pollfd_.size()) - 1;
+		channel->set_index(idx);
+		channels_[pfd.fd] = channel;
+	} else {
+		assert(channels_.find(channel->fd()) != channels_.end());
+		assert(channels_[channel->fd()] == channel);
+		int idx = channel->index();
+		assert(0 <= idx && idx < static_cast<int>pollfds_.size());
+		struct pollfd &pfd = pollfds_[idx];
+		assert(pfd.fd == channel->fd() || pfd.fd == -1);
+		pfd.events = static_cast<short>(channel->events());
+		pfd.revents = 0;
+		if (channel->isNoneEvent()) {
+			pfd.fd = -1;
 		}
 	}
 }
